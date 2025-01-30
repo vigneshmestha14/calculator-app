23c24b133766580290c081b74c893b8e7627b01c
Vignesh Mestha <111165616+vigneshmestha14@users.noreply.github.com>
2025-01-30 20:51:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,5 @@ def index():
             return render_template('index.html', result='Error')
     return render_template('index.html', result='')
 
-
 if __name__ == '__main__':
     app.run(debug=True)
